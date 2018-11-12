bb71297463a81b52246550f41a27c6f2e3f6c168
= <=>
2018-11-12 13:26:44 +0530

    loaded the data set..

--- 1.Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ 1.Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
+dataset = pd.read_csv('E:/machine_learning/machine_learning/1.Regression/Multiple_Linear_Regression/50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
